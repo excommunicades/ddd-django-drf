72142ac22fd23e84551a20c7274604a8cef55e19
excommunicades <daniilworkmail49@gmail.com>
2024-12-24 23:57:04 +0200

    Project start settings were divided

---
+++ DDD/DDD/context_sets/connections.py
@@ -0,0 +1,11 @@
+SECRET_KEY = 'django-insecure-d$p98pyx)_w@hdmujpef^hc$7u!pfk=p=+!ldgg!4@0@#w*d5^'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+ROOT_URLCONF = 'DDD.context.urls'
+
+WSGI_APPLICATION = 'DDD.context.wsgi.application'
+
+STATIC_URL = 'static/'
